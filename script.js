780241181d29099456eee26021ce33d1660ccab7
geo <ge.or.gi.o@hotmail.com>
2020-12-30 16:03:20 +0200

    Fixed animation + transition js & CSS, bullet point and use form remaining

--- script.js
+++ script.js
@@ -3,38 +3,70 @@ var secondPage = document.getElementsByClassName("page2");
 var thirdPage = document.getElementsByClassName("page3");
 
 
-function hideFirstPage() {
+//This functions allows to wait for the animation to finish before executing the next command
+function resolveAfterXMilliSecond(x) {
+  console.log("starting slow promise")
+  return new Promise(resolve => {
+    setTimeout(function() {
+      resolve("slow")
+      console.log("slow promise is done")
+    }, x)
+  })
+}
 
-  for(var i=0; i<firstPage.length; i++) {
+function hideFirstPage() {
+  for (var i = 0; i < firstPage.length; i++) {
     firstPage[i].style.display = "none";
   }
-  for(var i=0; i<secondPage.length; i++) {
+  for (var i = 0; i < secondPage.length; i++) {
     secondPage[i].style.display = "flex";
   }
-
 }
+
+function animateFirstPage() {
+  for (var i = 0; i < firstPage.length; i++) {
+    firstPage[i].classList.add("hide");
+  }
+}
+
 
 function hideSecondPage() {
-  
-  for(var i=0; i<secondPage.length; i++) {
+
+  for (var i = 0; i < secondPage.length; i++) {
     secondPage[i].style.display = "none";
   }
-  for(var i=0; i<thirdPage.length; i++) {
+  for (var i = 0; i < thirdPage.length; i++) {
     thirdPage[i].style.display = "flex";
   }
 
 }
 
-function undoPage(){
+function animateSecondPage() {
+  for (var i = 0; i < secondPage.length; i++) {
+    secondPage[i].classList.add("hide");
+  }
+}
 
-  for(var i=0; i<firstPage.length; i++) {
-    firstPage[i].style.display = "flex";
-  }
-  for(var i=0; i<secondPage.length; i++) {
-    secondPage[i].style.display = "flex";
-  }
-  for(var i=0; i<thirdPage.length; i++) {
-    thirdPage[i].style.display = "flex";
-  }
 
+
+function undoPage() {
+
+  location.reload();
+
+}
+
+async function animateRemoveFirst() {
+  //console.log('==SEQUENTIAL START==')
+  animateFirstPage();
+  const slow = await resolveAfterXMilliSecond(650)
+  //console.log(slow) // 2. this runs 1 second after 1.
+  hideFirstPage();
+}
+
+async function animateRemoveSecond() {
+  //console.log('==SEQUENTIAL START==')
+  animateSecondPage();
+  const slow = await resolveAfterXMilliSecond(650)
+  //console.log(slow) // 2. this runs 1 second after 1.
+  hideSecondPage();
 }
