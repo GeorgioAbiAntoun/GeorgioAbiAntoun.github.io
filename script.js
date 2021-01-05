0bcfa97a34cafbe85cb053e0299f8c6c84338745
geo <ge.or.gi.o@hotmail.com>
2021-01-05 23:32:09 +0200

    fixed constraint + padding/margin for container

--- script.js
+++ script.js
@@ -2,14 +2,31 @@ var firstPage = document.getElementsByClassName("page1");
 var secondPage = document.getElementsByClassName("page2");
 var thirdPage = document.getElementsByClassName("page3");
 
+//Declaring the elements where the user needs to input value
+var petName = document.getElementById("petName");
+var breedSelector = document.getElementById("breedSelector");
+var month = document.getElementById("month");
+var year = document.getElementById("year");
+var weightSelector = document.getElementById("weightSelector");
+
+//Declaring the error message for each input element
+petName.setCustomValidity("Please enter your pet's name");
+breedSelector.setCustomValidity('Please select a breed!');
+month.setCustomValidity('Please enter a month!');
+year.setCustomValidity('Please enter a year');
+weightSelector.setCustomValidity("Please enter your pet's weight!");
+
+
+
+
 
 //This functions allows to wait for the animation to finish before executing the next command
 function resolveAfterXMilliSecond(x) {
-  console.log("starting slow promise")
+  console.log("starting animation")
   return new Promise(resolve => {
-    setTimeout(function() {
+    setTimeout(function () {
       resolve("slow")
-      console.log("slow promise is done")
+      console.log("animation done")
     }, x)
   })
 }
@@ -46,37 +63,57 @@ function animateSecondPage() {
   }
 }
 
-function undoPage() {
-
-  location.reload();
-
-}
-
+//This function will validate input, animate the page and transition to the second page
 async function animateRemoveFirst() {
-  //console.log('==SEQUENTIAL START==')
-  animateFirstPage();
-  const slow = await resolveAfterXMilliSecond(650)
-  //console.log(slow) // 2. this runs 1 second after 1.
-  hideFirstPage();
+
+  if (petName.value.trim().length == 0) {
+
+    petName.reportValidity();
+  }
+  else if (breedSelector.value == "SelectBreed") {
+
+    breedSelector.reportValidity();
+
+  } else {
+    animateFirstPage();
+    const slow = await resolveAfterXMilliSecond(650)
+    hideFirstPage();
+  }
 }
 
+//This function will validate input, animate the page and transition to the third page
 async function animateRemoveSecond() {
-  //console.log('==SEQUENTIAL START==')
-  animateSecondPage();
-  const slow = await resolveAfterXMilliSecond(650)
-  //console.log(slow) // 2. this runs 1 second after 1.
-  hideSecondPage();
+
+  if (month.value == "") {
+
+    month.reportValidity();
+  }
+  else if (year.value == "") {
+
+    year.reportValidity();
+
+  }
+  else if (weightSelector.value == "WeightInLbs") {
+
+    weightSelector.reportValidity();
+
+  }
+  else {
+    animateSecondPage();
+    const slow = await resolveAfterXMilliSecond(650)
+    hideSecondPage();
+  }
+
+
 }
 
 //Hide/show the breed size selector based if the user selects Mixed Breed or not
-$(document).ready(function(){
-  $('#breedSelector').on('change', function() {
-    if ( this.value == 'MixedBreed')
-    {
+$(document).ready(function () {
+  $('#breedSelector').on('change', function () {
+    if (this.value == 'MixedBreed') {
       document.getElementById("breedSize").style.display = "flex";
     }
-    else  
-    {
+    else {
       document.getElementById("breedSize").style.display = "none";
     }
   });
